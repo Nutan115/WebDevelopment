b166c6d8c4e59b047a0a56c470a549a2bd483d84
Nutan115 <153013581+Nutan115@users.noreply.github.com>
2025-04-02 00:07:45 +0530

    Add files via upload

---
+++ newproject/src/main/java/newproject/LoginServlet.java
@@ -0,0 +1,43 @@
+package newproject;
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "admin");
+
+            String query = "SELECT * FROM users WHERE email=? AND password=?";
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setString(1, email);
+            pst.setString(2, password);
+            ResultSet rs = pst.executeQuery();
+
+            if (rs.next()) {
+                HttpSession session = request.getSession();
+                session.setAttribute("username", rs.getString("username"));
+                response.sendRedirect("home.jsp");
+            } else {
+                response.getWriter().print("Invalid credentials. Try again.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
